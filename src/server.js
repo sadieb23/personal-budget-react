e52de1fa4c291ded151393ae606c814085ab6c18
sadieb23 <sadiebalk3@gmail.com>
2024-02-22 16:31:09 -0500

    Added server.js

--- src/server.js
+++ src/server.js
@@ -1,109 +1,30 @@
 const express = require('express');
-const app = express();
-
-const jwt = require('jsonwebtoken');
-const { expressjwt: exjwt } = require('express-jwt');
-const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
 const path = require('path');
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
-    next();
-});
+app.use(cors());
+app.use('/', express.static('public'));
 
-
-
-const PORT = 3000;
-
-const secretKey = 'My super secret key';
-const jwtMW = exjwt({
-    secret: secretKey,
-    algorithms: ['HS256']
-});
-
-let users = [
-    {
-        id: 1,
-        username: 'fabio',
-        password: '123'
-    },
-    {
-        id: 2,
-        username: 'nolasco',
-        password: '456'
+// Function to read budget data from the JSON file
+const readBudgetData = () => {
+    const jsonFilePath = path.join(__dirname, 'budget-data.json');
+    try {
+        const jsonData = fs.readFileSync(jsonFilePath, 'utf8');
+        return JSON.parse(jsonData);
+    } catch (error) {
+        console.error(`Error reading JSON file: ${error.message}`);
+        return {};
     }
-];
+};
 
-app.post('/api/login', (req, res) => {
-    const { username, password } = req.body;
-   
-    for (let user of users) {
-        if (username == user.username && password == user.password) {
-            let token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '180s'});
-            res.json({
-                success: true,
-                err: null,
-                token
-            });
-            break;
-        }
-        else{
-            res.status(401).json({
-                success: false,
-                token: null,
-                err: 'Username or password is incorrect'
-            });
-        }
-    }
+app.get('/budget', (req, res) => {
+    const budgetData = readBudgetData();
+    res.json(budgetData);
 });
 
-app.get('/api/dashboard', jwtMW, (req, res) => {
-            res.json({
-                success: true,
-                myContent: 'Secret content that only logged in people can see!!!'
-            });
-
-        });
-
-
-        app.get('/api/prices', jwtMW, (req, res) => {
-            res.json({
-                success: true,
-                myContent: 'This is the price $3.99'
-            });
-
-        });
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.use(function (err, req, res, next) {
-    console.log(err.name === 'UnauthorizedError');
-    console.log(err);
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).json({
-            success: fail,
-            officialError: err,
-            err: 'Username or password is incorrect 2'
-        });
-    }
-    else {
-        next(err);
-    }
-
-});
-
-app.get('/api/settings', jwtMW, (req, res) => {
-    res.json({
-        success: true,
-        myContent: 'Settings page content that only logged in users can see!'
-    });
-});
-
-app.listen(PORT, () => {
-    console.log(`Serving on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`API served at http://localhost:${port}`);
 });
